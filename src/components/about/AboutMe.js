467e203b3d533a9af790f7761428e1c969ad1da9
Christian Gomez Martinez <gomez9289@gmail.com>
2021-03-21 18:04:45 -0400

    Styled the about me map image

--- src/components/about/AboutMe.js
+++ src/components/about/AboutMe.js
@@ -1,28 +1,59 @@
 import React from "react";
-import "./about.css"
+import "./about.css";
 import Map from "../../assets/images/map.png";
 
-function AboutMe (){
-    return(
-        <div>
-            <div className="container" style={{justifyContent: "center", display: "flex"}}>
-        <div className="card mb-8" style={{maxWidth: "1000px", margin: "10px", justifyContent: "center", display: "flex"}}>
-            <div className="row g-0">
-                <div className="col-md-4">
-                    <img src={Map} alt="" style={{width: "100%", height: "100%", display: "flex"}}></img>
-                </div>
-                <div className="col-md-8">
-                    <div className="card-body">
-                        <h5> A little about me</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porttitor libero augue, ut euismod augue mattis tincidunt. Donec pellentesque elit nec malesuada volutpat. Duis ipsum tortor, sollicitudin ut dui et, accumsan pulvinar sapien. Quisque in massa rhoncus purus suscipit blandit. Nulla laoreet risus et lorem rutrum vulputate. Mauris ex metus, mattis sed posuere eu, faucibus sed dolor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin in sapien sodales, bibendum risus sit amet, eleifend dui. Donec porta lacus erat, eget rutrum sem molestie non. Donec sed nulla eget nibh ultrices imperdiet. Etiam aliquet velit nulla, nec ornare dolor fringilla sit amet. Maecenas ullamcorper eu erat eget dictum. Pellentesque faucibus ultrices pellentesque. Aliquam scelerisque, mauris eu lacinia convallis, lectus urna scelerisque arcu, et interdum libero velit sed sem. Proin nec ante massa.</p>
-                    </div>
-                </div>
+function AboutMe() {
+  return (
+    <div>
+      <div
+        className="container"
+        style={{ justifyContent: "center", display: "flex" }}
+      >
+        <div
+          className="card mb-8"
+          style={{
+            maxWidth: "1000px",
+            margin: "10px",
+            justifyContent: "center",
+            display: "flex",
+          }}
+        >
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img
+                src={Map}
+                alt=""
+                style={{ width: "100%", height: "100%", display: "flex", paddingRight: "20px", paddingLeft: "20px", paddingBottom: "20px" }}
+              ></img>
             </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5> A little about me</h5>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+                  porttitor libero augue, ut euismod augue mattis tincidunt.
+                  Donec pellentesque elit nec malesuada volutpat. Duis ipsum
+                  tortor, sollicitudin ut dui et, accumsan pulvinar sapien.
+                  Quisque in massa rhoncus purus suscipit blandit. Nulla laoreet
+                  risus et lorem rutrum vulputate. Mauris ex metus, mattis sed
+                  posuere eu, faucibus sed dolor. Vestibulum ante ipsum primis
+                  in faucibus orci luctus et ultrices posuere cubilia curae;
+                  Proin in sapien sodales, bibendum risus sit amet, eleifend
+                  dui. Donec porta lacus erat, eget rutrum sem molestie non.
+                  Donec sed nulla eget nibh ultrices imperdiet. Etiam aliquet
+                  velit nulla, nec ornare dolor fringilla sit amet. Maecenas
+                  ullamcorper eu erat eget dictum. Pellentesque faucibus
+                  ultrices pellentesque. Aliquam scelerisque, mauris eu lacinia
+                  convallis, lectus urna scelerisque arcu, et interdum libero
+                  velit sed sem. Proin nec ante massa.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-        </div>
-        </div>
-    )
-
+      </div>
+    </div>
+  );
 }
 
-export default AboutMe
+export default AboutMe;
