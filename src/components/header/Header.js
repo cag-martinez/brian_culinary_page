7dee479619acbad733bfbad42d206494d89636e5
Christian Gomez Martinez <gomez9289@gmail.com>
2021-03-20 23:56:15 -0400

    Header background image

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,38 @@
 import React from "react";
+import Art from "../../assets/images/art.jpg";
+// import "./header.css"
 
-function Header(){
-    return(
-        <div>
-            <h1>This will be the header witht the image</h1>
-        </div>
-    )
+function Header() {
+  return (
+    <div>
+      <section
+        className="header"
+        //     style={{
+        //     height: "50vh",
+        //     backgroundImage: "Art",
+        //     backgroundSize: "cover",
+        //     backgroundPosition: "center",
+        //     backgroundRepeat: "no-repeat",
+        //     opacity: "75%",
+        //     clipPath: "polygon(100% 0, 100% 75%, 69% 100%, 0 85%, 0 0)",
+        //     backgroundAttachment: "fixed",
+        //   }}
+      >
+        <img
+          src={Art}
+          style={{
+            height: "50vh",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            opacity: "75%",
+            clipPath: "polygon(100% 0, 100% 75%, 69% 100%, 0 85%, 0 0)",
+            backgroundAttachment: "fixed",
+          }}
+        />
+      </section>
+    </div>
+  );
 }
 
 export default Header;
